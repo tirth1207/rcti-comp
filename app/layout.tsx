30b51883d5409082d23674b67f1b56857c000f59
Tirth <104121248+tirth1207@users.noreply.github.com>
2025-10-01 15:41:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -117,8 +117,8 @@ export default function RootLayout({
       >
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          // enableSystem
           disableTransitionOnChange
         >
           <NavConditional />
